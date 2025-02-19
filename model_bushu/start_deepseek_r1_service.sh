3d27c2feacecb6d43f1ea9bee8d71b9d0ae655b1
gaojiale <776856937@qq.com>
2025-02-19 10:36:19 +0800

    添加指定api-key，但要注意访问格式

--- model_bushu/start_deepseek_r1_service.sh
+++ model_bushu/start_deepseek_r1_service.sh
@@ -3,6 +3,10 @@
 # 设置要使用的 GPU 编号
 export CUDA_VISIBLE_DEVICES=2
 
+# 设置 API 密钥
+API_KEY="sk-1234567890"
+export VLLM_API_KEY=$API_KEY
+
 # 检查模型路径是否存在
 MODEL_PATH="/DATA/LLM_model/deepseek/DeepSeek-R1-Distill-Qwen-1.5B"
 if [ ! -d "$MODEL_PATH" ]; then
